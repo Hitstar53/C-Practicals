4187550253bfbf1a12e31dab2acc5e3d7068918f
Hatim Sawai <hatimsawai76@gmail.com>
2022-03-05 18:28:27 +0530

    prog3 of exp 6
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- C Practicals-SPIT/Experiment-6/prog3.c
+++ C Practicals-SPIT/Experiment-6/prog3.c
@@ -1,24 +1,64 @@
 #include<stdio.h>
-void print_mat(int m, int n, int mat[m][n])
+void print_mat(int n, int mat[n][n])
 {
-    for (int i = 0; i < m; i++)
+    for (int i=0;i<n;i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j=0;j<n;j++)
             printf("%d ", mat[i][j]);
         printf("\n");
     }
 }
+void cofact(int n, int mat[n][n], int cof[n-1][n-1], int r, int c)
+{
+    int i=0,j=0;
+    for(int row=0;row<n;row++)
+    {
+        for(int col=0;col<n;col++)
+        {
+            if(row!=r && col!=c)
+            {
+                cof[i][j++] = mat[row][col];
+                if(j==n-1)
+                {
+                    j=0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+int det_matrix(int n, int det[n][n])
+{
+    int d=0;
+    if(n==1)
+        return det[0][0];
+    int cof[n][n];
+    int sign=1;
+    for(int j=0;j<n;j++)
+    {
+        cofact(n,det,cof,0,j);
+        d += sign*det[0][j]*det_matrix(n-1,cof);
+        sign = -sign;
+    }
+    return d;
+}
 int main()
 {
-    int m,n,i,j;
-    printf("Enter dimensions of the matrix:\n");
-    scanf("%d %d",&m,&n);
-    int det[m][n];
-    for(i=0;i<m;i++)
+    int n,i,j;
+    printf("Enter dimension of the matrix:\n");
+    scanf("%d",&n);
+    int det[n][n];
+    printf("Enter Elements of Matrix:\n");
+    for(i=0;i<n;i++)
     {
         for(j=0;j<n;j++)
         scanf("%d",&det[i][j]);
     }
-    print_mat(m,n,det);
+    printf("Entered Matrix:\n");
+    print_mat(n,det);
+    if(det_matrix(n,det)==0)
+        printf("This matrix is singular\n");
+    else
+        printf("This matrix is not singular\nDeterminant = %d",det_matrix(n,det));
     return 0;
 }
