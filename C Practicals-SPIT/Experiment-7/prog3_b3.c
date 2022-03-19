cc50eb60a6377185ddd0ecbf96d81ad5e6f6b89e
Hatim Sawai <hatimsawai76@gmail.com>
2022-03-19 21:55:43 +0530

    prog3 of exp7
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- C Practicals-SPIT/Experiment-7/prog3_b3.c
+++ C Practicals-SPIT/Experiment-7/prog3_b3.c
@@ -1,6 +1,54 @@
 #include<stdio.h>
+#include<string.h>
+void replacestr(char str[],char wor[],char rep[])
+{
+    int stlen,worlen,replen;
+    int i,j,k;
+    int flag=0,start,end;
+    stlen = strlen(str);
+    worlen = strlen(wor);
+    replen = strlen(rep);
+    for (i=0;i<stlen;i++)
+    {
+        flag=0;
+        start=i;
+        for (j=0;str[i] == wor[j];j++,i++)
+            if (j == worlen-1)
+                flag = 1;
+        end = i;
+        if (flag == 0)
+            i -= j;
+        else
+        {
+            for (j=start;j<end;j++)
+            {
+                for (k=start;k<stlen;k++)
+                    str[k] = str[k + 1];
+                stlen--;
+                i--;
+            }
+            for (j=start;j<start+replen;j++)
+            {
+                for (k=stlen;k>=j;k--)
+                    str[k+1] = str[k];
+                str[j] = rep[j-start];
+                stlen++;
+                i++;
+            }
+        }
+    }
+}
+
 int main()
 {
-    
+    char str[100],wor[100],rep[100];
+    printf("Enter the sentance:\n");
+    scanf("%[^\n]s",str);
+    printf("Enter the word to be replaced:\n");
+    scanf(" %[^\n]s",wor);
+    printf("Enter the replacement word:\n");
+    scanf(" %[^\n]s",rep);
+    replacestr(str,wor,rep);
+    printf("%s\n",str);
     return 0;
 }
