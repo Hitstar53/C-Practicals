301d6e0f4888978d8356152ef745a2896e7768ae
Hatim Sawai <hatimsawai76@gmail.com>
2022-04-12 08:28:36 +0530

    10th exp done!
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- C Practicals-SPIT/Experiment-10/prog3.c
+++ C Practicals-SPIT/Experiment-10/prog3.c
@@ -2,20 +2,39 @@
 #include<string.h>
 int replace(char file[])
 {
-    FILE *fp;
-    int n;
-    char line[100];
-    fp = fopen(file, "a+");
-    if (fp == NULL)
+    FILE *fp,*fp1;
+    int n,c=0;
+    char buffer[100],newline[100];
+    printf("Enter line number: ");
+    scanf("%d",&n);
+    printf("Enter new line: ");
+    scanf(" %[^\n]s",newline);
+
+    fp = fopen(file,"r");
+    fp1 = fopen("temp.txt","w");
+    if (fp == NULL || fp1 == NULL)
     {
         printf("File not found!");
         return 0;
     }
-    printf("Enter line number: ");
-    scanf("%d",&n);
-    printf("Enter new line: ");
-    scanf(" %[^\n]s",line);
 
+    while(fgets(buffer,100,fp)!=NULL)
+    {
+        c++;
+        if(c==n)
+        {
+            fputs(newline,fp1);
+            fputs("\n",fp1);
+        }
+        else
+            fputs(buffer,fp1);
+    }
+    fclose(fp);
+    fclose(fp1);
+    remove(file);
+    rename("temp.txt",file);
+    printf("Successfully replaced new line with %d !",n);
+    return 0;
 }
 int main()
 {
